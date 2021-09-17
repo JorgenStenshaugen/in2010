cf3527468b62035c09fb903409a398379cbb8a62
Yahya Isam <yahya_isam@hotmail.com>
2021-09-17 15:39:48 +0200

    Oppgave 4b

---
+++ Oblig 1/oppg4b.py
@@ -0,0 +1,34 @@
+from heapq import heappop, heappush
+
+import sys
+
+array = []
+
+def array_to_bst(right_side):
+    if len(right_side) == 1:
+        print(heappop(right_side))
+    
+    if len(right_side) > 1:
+        middle_index = len(right_side) // 2
+        
+        left_side = []
+        for e in range(middle_index):
+            right = heappop(right_side)
+            heappush(left_side, right)
+
+        print(heappop(right_side))
+
+        array_to_bst(right_side)
+        array_to_bst(left_side)
+
+for linje in sys.stdin:
+
+    try:
+        user_input = int(linje.rstrip())
+        heappush(array, user_input)
+
+    except:
+        array_to_bst(array)
+        array = []
+
+array_to_bst(array)
