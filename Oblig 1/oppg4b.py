a8379c2e236f64ac435b656d3df8ad6d9e764fd7
yahyaisam <90194525+yahyaisam@users.noreply.github.com>
2021-09-17 15:52:08 +0200

    Update oppg4b.py

--- Oblig 1/oppg4b.py
+++ Oblig 1/oppg4b.py
@@ -4,9 +4,9 @@ import sys
 
 array = []
 
-def array_to_bst(right_side):
-    if len(right_side) == 1:
-        print(heappop(right_side))
+def array_to_bst(right_side):    
+    if not len(right_side):
+        return
     
     if len(right_side) > 1:
         middle_index = len(right_side) // 2
@@ -20,9 +20,11 @@ def array_to_bst(right_side):
 
         array_to_bst(right_side)
         array_to_bst(left_side)
+        
+    else:
+        print(heappop(right_side))
 
 for linje in sys.stdin:
-
     try:
         user_input = int(linje.rstrip())
         heappush(array, user_input)
