00c6caff1ad322df7a812776ff1cb7dcaa8d2dea
leJayJ <jorgenstenshaugen@gmail.com>
2021-10-04 14:07:49 +0200

    Added strcutre to oblig2

---
+++ Oblig 2/filmgraf.py
@@ -0,0 +1,25 @@
+def FilmGraf:
+	def __init__( self ):
+		return None
+	
+	def readActorFile( self ):
+		return None
+	
+	def readMoviesFile( self ):
+		return None
+	
+	def countNodesEdges( self ):
+		print( "Oppgave 1:\n" )
+		print( "Nodes:", 0 )
+		print( "Edges:", 0 )
+	
+	def findShortestPath( self ):
+		movie = None
+		actor = None
+		print( "===[", movie, "] ===>", actor )
+
+	def findChillestPath( self ):
+		movie = None
+		actor = None
+		print( "===[", movie, "] ===>", actor )
+	
