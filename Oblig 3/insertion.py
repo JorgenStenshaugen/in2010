719093efea5da62b3157500d1d486f50d9b2bfe3
Jørgen Stenshaugen <jorgenstenshaugen@gmail.com>
2021-11-02 14:09:57 +0100

    Lagt til prekode

---
+++ Oblig 3/insertion.py
@@ -0,0 +1,13 @@
+# Input: Et array med n elementer
+# Output: Et sortert array med de samme n elementene
+
+# O(n) * O(n) = O(n^2) tid
+# Spesielt rask på 'nesten sorterte' arrayer - blant de raskeste algoritmene for små arrayer
+def sort(array):
+    for i in range(len(array)):
+        j = i
+        while j > 0 and array[j - 1] > array[j]:
+            array[j - 1], array[j] = array[j], array[j - 1]
+            j = j - 1
+    
+    return array
---
+++ Oblig 3/quick.py
@@ -0,0 +1,35 @@
+# Input: Et array med n elementer, low og high er indekser
+# Output: Flytter elementer som er henholdsvis mindre og større til venstre og høyre enn en gitt indeks som returneres
+def partition(array, low, high):
+	pivot = (low + high) // 2
+
+	array[pivot], array[high] = array[high], array[pivot]
+	pivot = array[high]
+	left = low
+	right = high - 1
+
+	while left <= right:
+		while left <= right and array[left] <= pivot:
+			left = left + 1
+
+		while right >= left and array[right] >= pivot:
+			right = right - 1
+
+		if left < right:
+			array[left], array[right] = array[right], array[left]
+
+	array[left], array[high] = array[high], array[left]
+
+	return left
+
+# Beste tilfelle: O(n log(n)) tid hvor pivot er midt i mellom low og high, da halverer vi arbeidet for hvert rekursive kall
+# Verste tilfelle: O(n^2) tid men dette skjer sjeldent som vil si at quicksort er som regel veldig effektivt
+def sort(array, low, high):
+	if low >= high:
+		return array
+
+	pivot = partition(array, low, high)
+	quickSort(array, low, pivot - 1)
+	quickSort(array, pivot + 1, high)
+
+	return array
